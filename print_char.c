c7988e1724e7a71a7866a771da7962dcf3612d06
Talk2royal <inaanianiatambari@gmail.com>
2023-01-22 13:30:39 -0800

    funtion to print char

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "header.h"
+
+/**
+ * print_char - print out character
+ * @character:arg to be printed
+ *
+ * Return: void
+ */
+
+void print_char(va_list character)
+{
+	int cha = va_arg(character, int);
+	char p = (char)cha;
+	write(1, &p, 1);
+}	
